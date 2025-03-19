07f8307d554717ef71d45eaaba9f8e633a1b6031
Anton Khorev <tony29@yandex.ru>
2025-03-19 04:19:50 +0300

    Provide part of URL interface

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict"
+
+export class GeoURL {
+	constructor(urlString) {
+		this.url = new URL(urlString)
+		if (this.url.protocol != "geo:") {
+			throw new TypeError(`Invalid protocol ${this.url.protocol}`)
+		}
+	}
+
+	toJSON() {
+		return this.url.toJSON()
+	}
+	toString() {
+		return this.url.toString()
+	}
+
+	get href() {
+		return this.url.href
+	}
+	get pathname() {
+		return this.url.pathname
+	}
+	get protocol() {
+		return this.url.protocol
+	}
+	get search() {
+		return this.url.search
+	}
+	get searchParams() {
+		return this.url.searchParams
+	}
+}
---
+++ index.test.js
@@ -0,0 +1,51 @@
+import { describe, test, expect } from "vitest"
+
+import { GeoURL } from "./index.js"
+
+describe("GeoURL", () => {
+	test("Fails when no parameters given", () => {
+		expect(
+			() => new GeoURL()
+		).toThrow(TypeError)
+	})
+	test("Fails on empty string", () => {
+		expect(
+			() => new GeoURL("")
+		).toThrow(TypeError)
+	})
+	test("Fails on normal url", () => {
+		expect(
+			() => new GeoURL("https://www.example.com/")
+		).toThrow(TypeError)
+	})
+
+	test("Converts to json", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.toJSON()).toBe("geo:47.6,-122.3?z=11")
+	})
+	test("Converts to string", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.toString()).toBe("geo:47.6,-122.3?z=11")
+	})
+
+	test("Provides href getter", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.href).toBe("geo:47.6,-122.3?z=11")
+	})
+	test("Provides pathname getter", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.pathname).toBe("47.6,-122.3")
+	})
+	test("Provides protocol getter", () => {
+		const url = new GeoURL("geo:13.4125,103.8667")
+		expect(url.protocol).toBe("geo:")
+	})
+	test("Provides search getter", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.search).toBe("?z=11")
+	})
+	test("Provides searchParams getter", () => {
+		const url = new GeoURL("geo:47.6,-122.3?z=11")
+		expect(url.searchParams).toStrictEqual(new URLSearchParams({ z: 11 }))
+	})
+})
