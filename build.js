7c0d81ca8afee76ede9ac2d00e5c0c00357f0f52
Anton Khorev <tony29@yandex.ru>
2025-04-22 04:30:55 +0300

    Add marker to Leaflet map demo

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ import * as fs from "fs/promises"
 await cleanupDirectory("pages")
 await fs.copyFile("node_modules/leaflet/dist/leaflet.css", "pages/leaflet.css")
 await fs.copyFile("node_modules/leaflet/dist/leaflet.js", "pages/leaflet.js")
+await fs.cp("node_modules/leaflet/dist/images", "pages/images", { recursive: true })
 await fs.copyFile("index.js", "pages/geo-url.js")
 await fs.cp("demos", "pages/demos", { recursive: true })
 
