a132451f6c89839a0e79f24208f68eea4fd263b0
Anton Khorev <tony29@yandex.ru>
2025-04-23 16:22:13 +0300

    Generate jsdoc html

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 import * as fs from "fs/promises"
+import { spawn } from "child_process"
 
 await cleanupDirectory("pages")
+await generateJsdoc("pages")
 await fs.copyFile("node_modules/leaflet/dist/leaflet.css", "pages/leaflet.css")
 await fs.copyFile("node_modules/leaflet/dist/leaflet.js", "pages/leaflet.js")
 await fs.cp("node_modules/leaflet/dist/images", "pages/images", { recursive: true })
@@ -10,6 +12,14 @@ await fs.cp("demos", "pages/demos", { recursive: true })
 async function cleanupDirectory(dir) {
 	await fs.mkdir(dir, { recursive: true })
 	for (const filename of await fs.readdir(dir)) { // delete contents instead of the whole dir because live server doesn't like the dir disappearing
-		await fs.rm(`${dir}/${filename}`,{ recursive: true, force: true })
+		await fs.rm(`${dir}/${filename}`, { recursive: true, force: true })
 	}
+}
+
+function generateJsdoc(dir) {
+	const child = spawn("npx", ["jsdoc", "index.js", "-d", dir])
+	return new Promise((resolve, reject) => {
+		child.on('error', reject)
+		child.on('close', code => code == 0 ? resolve() : reject())
+	})
 }
