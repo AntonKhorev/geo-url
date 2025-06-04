96992a0e26af26da587577f166f5bed4b96cec56
Anton Khorev <tony29@yandex.ru>
2025-06-04 17:30:32 +0300

    Show marker on MapLibre map

--- demos/maplibre/index.js
+++ demos/maplibre/index.js
@@ -7,3 +7,18 @@ const map = new maplibregl.Map({
 	zoom: 13,
 	container: "map"
 })
+
+let marker
+
+const $geoUriInput = document.getElementById("geo-uri-input")
+
+$geoUriInput.oninput = updateMap
+updateMap()
+
+function updateMap() {
+	marker?.remove()
+	try {
+		const url = new WGS84GeoURL($geoUriInput.value)
+		marker = new maplibregl.Marker().setLngLat(url.lngLat).addTo(map)
+	} catch {}
+}
