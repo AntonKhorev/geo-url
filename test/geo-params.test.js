93cdce9fad14c228d509f9e513b61ccfce017bdb
Anton Khorev <tony29@yandex.ru>
2025-06-07 07:55:01 +0300

    Split test file

---
+++ test/geo-params.test.js
@@ -0,0 +1,67 @@
+import { describe, test, expect } from "vitest"
+
+import { GeoParams } from "../index.js"
+
+describe("GeoParams", () => {
+	test("Converts empty params to a string", () => {
+		const params = new GeoParams("")
+		expect(params.toString()).toBe("")
+	})
+	test("Converts params to a string", () => {
+		const params = new GeoParams("hello=world;yes;year=2025")
+		expect(params.toString()).toBe("hello=world;yes;year=2025")
+	})
+
+	test("Gets a null value for a parameter from empty string", () => {
+		const params = new GeoParams("")
+		expect(params.get("foo")).toBeNull()
+	})
+	test("Gets a null value for a missing parameter from a single-parameter string", () => {
+		const params = new GeoParams("foo=42")
+		expect(params.get("bar")).toBeNull()
+	})
+	test("Gets a parameter value from a single-parameter string", () => {
+		const params = new GeoParams("foo=42")
+		expect(params.get("foo")).toBe("42")
+	})
+	test("Gets a value with '&'", () => {
+		const params = new GeoParams("foo=bar&baz")
+		expect(params.get("foo")).toBe("bar&baz")
+		expect(params.get("baz")).toBeNull()
+	})
+	test("Gets a value with multiple '&'", () => {
+		const params = new GeoParams("foo=bar&baz&zab&rab")
+		expect(params.get("foo")).toBe("bar&baz&zab&rab")
+		expect(params.get("baz")).toBeNull()
+		expect(params.get("zab")).toBeNull()
+		expect(params.get("rab")).toBeNull()
+	})
+	test("Preserves the value case", () => {
+		const params = new GeoParams("foo=BaR")
+		expect(params.get("foo")).toBe("BaR")
+	})
+	test("Preserves the value case of a crs param", () => {
+		const params = new GeoParams("crs=WGS84")
+		expect(params.get("crs")).toBe("WGS84")
+	})
+	test("Gets a value of a param with a mixed-case name when requesting in lowercase", () => {
+		const params = new GeoParams("FoO=bar")
+		expect(params.get("foo")).toBe("bar")
+	})
+	test("Gets a value of a param with a mixed-case name when requesting in uppercase", () => {
+		const params = new GeoParams("FoO=bar")
+		expect(params.get("FOO")).toBe("bar")
+	})
+	test("Gets a flag parameter", () => {
+		const params = new GeoParams("flag")
+		expect(params.get("flag")).toBe("")
+	})
+	test("Doesn't decode '+' in a value", () => {
+		const params = new GeoParams("plus=+")
+		expect(params.get("plus")).toBe("+")
+	})
+	test("Percent-decodes a value", () => {
+		const params = new GeoParams("decode=%31%32%33")
+		expect(params.get("decode")).toBe("123")
+	})
+})
---
+++ test/wgs84-geo-url.test.js
@@ -0,0 +1,109 @@
+import { describe, test, expect } from "vitest"
+
+import { WGS84GeoURL } from "../index.js"
+
+describe("WGS84GeoURL", () => {
+	test("Fails when no parameters given", () => {
+		expect(
+			() => new WGS84GeoURL()
+		).toThrow(TypeError)
+	})
+	test("Fails on non-matching crs", () => {
+		expect(
+			() => new WGS84GeoURL("geo:0,0;crs=unknown")
+		).toThrow(TypeError)
+	})
+	test("Rejects lat below the allowed range in WGS84", () => {
+		expect(
+			() => new WGS84GeoURL("geo:-91,0")
+		).toThrow(TypeError)
+	})
+	test("Rejects lat above the allowed range in WGS84", () => {
+		expect(
+			() => new WGS84GeoURL("geo:91,0")
+		).toThrow(TypeError)
+	})
+	test("Rejects lon below the allowed range in WGS84", () => {
+		expect(
+			() => new WGS84GeoURL("geo:0,-181")
+		).toThrow(TypeError)
+	})
+	test("Rejects lon above the allowed range in WGS84", () => {
+		expect(
+			() => new WGS84GeoURL("geo:0,181")
+		).toThrow(TypeError)
+	})
+	test("Succeeds on mixed-case crs", () => {
+		const url = new WGS84GeoURL("geo:0,0;CrS=WgS84")
+		expect(url.crs).toBe("wgs84")
+	})
+	test("Allows to add hash to base geo uri", () => {
+		const url = new WGS84GeoURL("#hash", "geo:12,34")
+		expect(url.toString()).toBe("geo:12,34#hash")
+	})
+
+	test("Parses valid geo URI", () => {
+		const url = WGS84GeoURL.parse("geo:12,34")
+		expect(url).toBeInstanceOf(WGS84GeoURL)
+		expect(url.toString()).toBe("geo:12,34")
+	})
+	test("Parses valid non-WGS84 geo URL", () => {
+		const url = WGS84GeoURL.parse("geo:0,0;crs=unknown")
+		expect(url).toBeNull()
+	})
+
+	test("Checks if can parse valid geo URI", () => {
+		expect(WGS84GeoURL.canParse("geo:12,34")).toBe(true)
+
+	})
+	test("Checks if can parse valid non-WGS84 geo URL", () => {
+		expect(WGS84GeoURL.canParse("geo:0,0;crs=unknown")).toBe(false)
+	})
+
+	test("Has latLon/lonLat/*lng array properties for simple 2-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:13.4125,103.8667")
+		expect(url.latLon).toStrictEqual([13.4125, 103.8667])
+		expect(url.latLng).toStrictEqual([13.4125, 103.8667])
+		expect(url.lonLat).toStrictEqual([103.8667, 13.4125])
+		expect(url.lngLat).toStrictEqual([103.8667, 13.4125])
+	})
+	test("Has latLon/lonLat/*lng array properties for simple 3-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:48.2010,16.3695,183")
+		expect(url.latLon).toStrictEqual([48.2010, 16.3695])
+		expect(url.latLng).toStrictEqual([48.2010, 16.3695])
+		expect(url.lonLat).toStrictEqual([16.3695, 48.2010])
+		expect(url.lngLat).toStrictEqual([16.3695, 48.2010])
+	})
+
+	test("Has lat and lon/lng properties for simple 2-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:13.4125,103.8667")
+		expect(url.lat).toBe(13.4125)
+		expect(url.lon).toBe(103.8667)
+		expect(url.lng).toBe(103.8667)
+	})
+	test("Has latitude and longitude properties for simple 2-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:13.4125,103.8667")
+		expect(url.latitude).toBe(13.4125)
+		expect(url.longitude).toBe(103.8667)
+	})
+	test("Has undefined alt property for simple 2-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:13.4125,103.8667")
+		expect(url.alt).toBeUndefined()
+	})
+	test("Has undefined altitude property for simple 2-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:13.4125,103.8667")
+		expect(url.altitude).toBeUndefined()
+	})
+	test("Has alt property for simple 3-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:48.2010,16.3695,183")
+		expect(url.alt).toBe(183)
+	})
+	test("Has altitude property for simple 3-coordinate url", () => {
+		const url = new WGS84GeoURL("geo:48.2010,16.3695,183")
+		expect(url.altitude).toBe(183)
+	})
+	test("Has undefined alt property for empty alt value", () => {
+		const url = new WGS84GeoURL("geo:0,0,")
+		expect(url.alt).toBeUndefined()
+	})
+})
