a1e71ad57dd77a635e057e3c2d57e0be8e95869b
Anton Khorev <tony29@yandex.ru>
2025-06-16 10:48:26 +0300

    Group multiple examples using describe() in jsdoc tests

--- test-jsdoc.js
+++ test-jsdoc.js
@@ -1,6 +1,6 @@
 import vm from "node:vm"
 import assert from "node:assert"
-import test from "node:test"
+import { describe, test } from "node:test"
 import jsdoc from "jsdoc-api"
 
 import { GeoURL, WGS84GeoURL } from "./dist/index.js"
@@ -10,44 +10,56 @@ const data = await jsdoc.explain({ files: ["dist/index.js"] })
 for (const item of data) {
 	if (!item.examples) continue
 
-	for (const [i, example] of item.examples.entries()) {
-		test(`${item.longname} > Example ${i}`, () => {
-			let code = ""
-			for (const line of example.split("\n")) {
-				let match
-				if (match = line.match(new RegExp(`//\\s*outputs\\s+"(.*)"\s*$`))) {
-					const [, expectedString] = match
-					code += line + "\n"
-					code += `_assertLastConsoleArg("${expectedString}")\n`
-				} else if (match = line.match(new RegExp(`//\\s*throws.*$`))) {
-					code += `_assertThrows(() => {\n`
-					code += line + "\n"
-					code += `})\n`
-				} else {
-					code += line + "\n"
-				}
+	if (item.examples.length == 1 ) {
+		test(item.longname, () => {
+			testExample(item.examples[0])
+		})
+	} else {
+		describe(item.longname, () => {
+			for (const [i, example] of item.examples.entries()) {
+				test(`Example ${i}`, () => {
+					testExample(example)
+				})
 			}
-
-			let lastConsoleArg
-			const context = {
-				GeoURL, WGS84GeoURL,
-				L: { marker: () => {} }, // skip Leaflet code
-				console: {
-					log: arg => { lastConsoleArg = arg }
-				},
-				_assertLastConsoleArg: expectedOutput => {
-					if (expectedOutput == "undefined") {
-						assert.equal(lastConsoleArg, undefined)
-					} else if (expectedOutput[0] == "[") {
-						assert.deepEqual(lastConsoleArg, JSON.parse(expectedOutput))
-					} else {
-						assert.equal(lastConsoleArg, expectedOutput)
-					}
-				},
-				_assertThrows: fn => assert.throws(fn)
-			}
-			vm.createContext(context)
-			vm.runInContext(code, context)
 		})
 	}
+}
+
+function testExample(example) {
+	let code = ""
+	for (const line of example.split("\n")) {
+		let match
+		if (match = line.match(new RegExp(`//\\s*outputs\\s+"(.*)"\s*$`))) {
+			const [, expectedString] = match
+			code += line + "\n"
+			code += `_assertLastConsoleArg("${expectedString}")\n`
+		} else if (match = line.match(new RegExp(`//\\s*throws.*$`))) {
+			code += `_assertThrows(() => {\n`
+			code += line + "\n"
+			code += `})\n`
+		} else {
+			code += line + "\n"
+		}
+	}
+
+	let lastConsoleArg
+	const context = {
+		GeoURL, WGS84GeoURL,
+		L: { marker: () => {} }, // skip Leaflet code
+		console: {
+			log: arg => { lastConsoleArg = arg }
+		},
+		_assertLastConsoleArg: expectedOutput => {
+			if (expectedOutput == "undefined") {
+				assert.equal(lastConsoleArg, undefined)
+			} else if (expectedOutput[0] == "[") {
+				assert.deepEqual(lastConsoleArg, JSON.parse(expectedOutput))
+			} else {
+				assert.equal(lastConsoleArg, expectedOutput)
+			}
+		},
+		_assertThrows: fn => assert.throws(fn)
+	}
+	vm.createContext(context)
+	vm.runInContext(code, context)
 }
